d8076e31ff147789254f846c6b705671453330dd
Eldar Mavlikhanov <emavlikhanov@oggettoweb.com>
2021-09-14 19:14:12 +0300

    F

--- app/Providers/MicroMarkup/MicroMarkupProduct.php
+++ app/Providers/MicroMarkup/MicroMarkupProduct.php
@@ -33,10 +33,6 @@ class MicroMarkupProduct implements IMicroMarkup
         foreach ($comments as $comment){
             $comments_markup[] = Schema::review()
                 ->name("Отзыв")
-                ->aggregateRating(
-                    Schema::aggregateRating()
-                    ->ratingValue(5)
-                )
                 ->author($comment->name)
                 ->description($comment->text)
                 ->datePublished($comment->created_at->format('Y-m-d'));
@@ -51,6 +47,10 @@ class MicroMarkupProduct implements IMicroMarkup
             }
         }
         $this->Markup = Schema::product()
+            ->aggregateRating(
+                Schema::aggregateRating()
+                ->ratingValue(5)
+            )
             ->name($product->name)
             ->image($images_list)
             ->brand([$category->name])
